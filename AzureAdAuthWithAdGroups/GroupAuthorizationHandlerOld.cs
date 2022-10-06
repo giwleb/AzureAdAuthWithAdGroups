38ce4e1cd8c408d171d689b80106db7b59789e52
John Hutchinson <junk@hutchinthehills.com>
2022-10-06 12:04:48 -0400

    initial

---
+++ AzureAdAuthWithAdGroups/GroupAuthorizationHandlerOld.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AzureAdAuthWithAdGroups
+{
+    public class GroupAuthorizationHandlerOld : AuthorizationHandler<GroupAuthorizationRequirementOld>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, GroupAuthorizationRequirementOld requirement)
+        {
+            var x = context.User.Claims;
+
+            if (context.User.Claims.Any(c=>c.Type=="groups" && (c.Value==requirement.GroupIdDev || c.Value == requirement.GroupIdTest || c.Value == requirement.GroupIdQa || c.Value == requirement.GroupIdProd)))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
